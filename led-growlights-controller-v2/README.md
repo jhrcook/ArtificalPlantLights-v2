cb31e77668efd25b29993054124604a07e3e60f1
Josh Cook <joshuacook0023@gmail.com>
2020-06-07 11:45:32 -0400

    feat: final lights setup; updated README

--- led-growlights-controller-v2/README.md
+++ led-growlights-controller-v2/README.md
@@ -3,6 +3,10 @@
 **author: Joshua Cook**  
 **date: 2020-05-31**
 
-The main code file is [`led_growlights_controller_v2.ino`](led_growlights_controller_v2.ino).
+[![arduino](https://img.shields.io/badge/Arduino-sketch-00979D.svg?style=flat&logo=arduino)](https://www.arduino.cc)
 
-I created a class, `GrowLight`, defined in [GrowLight.h](GrowLight.h) and [GrowLight.cpp](GrowLight.cpp) to keep track and coordinate each set of grow lights.
+The Arduino sketch [`led_growlights_controller_v2.ino`](led_growlights_controller_v2.ino) controls the LED light system.
+Each light system is controlled by a separate relay switch connecting the LED driver and AC mains.
+Each relay is controlled by a [`sunGrowLight`](https://github.com/jhrcook/sunGrowLight) object that I made as a separate library.
+This object takes the relay pin to control and the latitude, longitude, and time zone of a location to from which to use the sunrise and sunset times to turn the lights on and off.
+The calculation is handled by the [`sfrwmaker/sunMoon`](https://github.com/sfrwmaker/sunMoon) Arduino library.
--- led-growlights-controller-v2/led_growlights_controller_v2.ino
+++ led-growlights-controller-v2/led_growlights_controller_v2.ino
@@ -1,9 +1,26 @@
 #include <Wire.h>
 #include <RTClib.h>
-#include "GrowLight.h"
+#include <Time.h>
+#include <sunGrowLight.h>
 
 // initiate RTC module object
 RTC_DS3231 rtc;
+
+// Johannesburg, South Africa (in Northern hemisphere)
+#define SA_LAT 26.195246
+#define SA_LONG 28.034088
+#define SA_TZ 120
+
+// Joshua Tree National Park, California, USA
+#define JT_LAT 33.881866
+#define JT_LONG -115.900650
+#define JT_TZ -420
+
+// Los Angeles, California, USA
+#define LA_LAT 34.052235
+#define LA_LONG -118.243683
+#define LA_TZ -420
+
 
 // IO pins
 int relayOne = 10;
@@ -14,18 +31,22 @@ int buttonTwo = 3;
 int buttonThree = 4;
 
 // self-defined grow-lights object
-GrowLight lightOne(relayOne, 8, 21);
-GrowLight lightTwo(relayTwo, 8, 21);
-GrowLight lightThree(relayThree, 8, 21);
+sunGrowLight lightOne(relayOne, SA_LAT, SA_LONG, SA_TZ);
+sunGrowLight lightTwo(relayTwo, JT_LAT, JT_LONG, JT_TZ);
+sunGrowLight lightThree(relayThree, LA_LAT, LA_LONG, LA_TZ);
 
 void setup() {
+
+//    Serial.begin(9600);
+//    Serial.print("Setting up... ");
+    
     pinMode(LED_BUILTIN, OUTPUT);
     
     // begin RTC
     rtc.begin();
     
-    if (rtc.lostPower()) {
-        Serial.println("RTC lost power, lets set the time!");
+    if (true) {
+        Serial.println("Reseting RTC time and date.");
         // following line sets the RTC to the date & time this sketch was compiled
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     }
@@ -35,21 +56,26 @@ void setup() {
     pinMode(buttonTwo, INPUT_PULLUP);
     pinMode(buttonThree, INPUT_PULLUP);
 
-    Serial.begin(9600);
-
     // pause before starting to control the lights
-    // delay(3000);
-    delay(300);
+    delay(3000);
+
+//    Serial.println("done.");
 }
 
 void loop() {
     // Retrieve the current time.
-    DateTime now = rtc.now();
+    DateTime now_dt = rtc.now();
+
+//    Serial.print("Current date time: ");
+//    Serial.println(now_dt.year());
+//    Serial.print("Current unixtime date time: ");
+//    printDate(now_dt.unixtime());
+//    Serial.println("");
     
     // Check the current time against the time limits
-    lightOne.updateLights(now.hour());
-    lightTwo.updateLights(now.hour());
-    lightThree.updateLights(now.hour());
+    lightOne.updateLights(now_dt.unixtime());
+    lightTwo.updateLights(now_dt.unixtime());
+    lightThree.updateLights(now_dt.unixtime());
     
     // Check time every 15 sec, otherwise look for button presses
     int checkTimeDelay = 2000;
@@ -58,9 +84,9 @@ void loop() {
     while(end_timer >= start_timer && checkTimeDelay > (end_timer - start_timer)) {
 
         if (
-            checkButtonForLight(buttonOne, &lightOne) == LOW ||
-            checkButtonForLight(buttonTwo, &lightTwo) == LOW ||
-            checkButtonForLight(buttonThree, &lightThree) == LOW
+            checkButtonForLight(buttonOne, &lightOne, now_dt) == LOW ||
+            checkButtonForLight(buttonTwo, &lightTwo, now_dt) == LOW ||
+            checkButtonForLight(buttonThree, &lightThree, now_dt) == LOW
         ) {
             delay(500);
             break;
@@ -69,20 +95,16 @@ void loop() {
         delay(50);
         end_timer = millis();
     }
-    
-    Serial.println("(end of cycle)");
+//    Serial.println("(end of cycle)");
 }
 
 
 // Check if the `button` is pressed. If it is, toggle the
 // lights `gl` override switch.
-int checkButtonForLight(int button, GrowLight *gl) {
+int checkButtonForLight(int button, sunGrowLight *gl, DateTime now_dt) {
     int buttonState = digitalRead(button);
     if (buttonState == LOW) {
-            // Serial.print("button pressed: ");
-            // Serial.println(button);
-            DateTime now = rtc.now();
-            gl->overrideLights(now.hour());
+            gl->overrideLights(now_dt.unixtime());
         }
     return buttonState;
 }
